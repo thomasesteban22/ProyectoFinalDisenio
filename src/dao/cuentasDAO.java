9026e2eb1bd7098f240e6767c1d5b4740c027be6
thomasesteban22 <thomas.esteban@gmail.com>
2022-11-02 17:03:17 -0500

    dao listas

---
+++ src/dao/cuentasDAO.java
@@ -0,0 +1,20 @@
+package dao;
+import data.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class cuentasDAO {
+    private List<Cuenta> listaCuentas;
+    public cuentasDAO(){ listaCuentas = new ArrayList<>();}
+
+    public boolean adicionar (Cuenta cuenta){return listaCuentas.add(cuenta);}
+    public boolean borrar (Cuenta cuenta){return listaCuentas.remove(cuenta);}
+
+    public Cuenta buscar(int numeroDeCuenta){
+
+
+
+    };
+
+}
--- src/data/IDepositar.java
+++ src/data/IDepositar.java
@@ -2,4 +2,6 @@ package data;
 
 public interface IDepositar {
     public void depositar(long valorDeposito);
+
+
 }
