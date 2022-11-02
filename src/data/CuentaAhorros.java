439ef521729ff8e1c176286ca4fed96d449e85bc
Manuel <manuel.castro01@correo.usa.edu.co>
2022-11-02 16:20:59 -0500

    interfases

--- src/data/CuentaAhorros.java
+++ src/data/CuentaAhorros.java
@@ -1,12 +1,9 @@
 package data;
 
-import java.time.LocalDate;
-
 public class CuentaAhorros extends Cuenta{
     private long interesAcomulado;
 
-    public CuentaAhorros(int numeroDeCuenta, LocalDate fechaDeApertura, Persona datosPersona, long dato, long interesAcomulado) {
-        super(numeroDeCuenta, fechaDeApertura, datosPersona, dato);
+    public CuentaAhorros(long interesAcomulado) {
         this.interesAcomulado = interesAcomulado;
     }
 }
