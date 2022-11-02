ef6c3eee07b51af7c776f16f7fbe2d7189c313b3
thomasesteban22 <thomas.esteban@gmail.com>
2022-11-02 16:12:38 -0500

    creacion de Cuenta.java

--- src/data/Cuenta.java
+++ src/data/Cuenta.java
@@ -1,4 +1,10 @@
 package data;
 
+import java.time.LocalDate;
+
 public class Cuenta {
+    private int numeroDeCuenta;
+    private LocalDate fechaDeApertura;
+    private Persona datosPersona;
+    private long dato;
 }
