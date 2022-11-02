fde6c94bad7138f39d80dc9fa6c4bea3491d3747
thomasesteban22 <thomas.esteban@gmail.com>
2022-11-02 16:15:02 -0500

    creacion de Cuenta.java

--- src/data/CuentaVivienda.java
+++ src/data/CuentaVivienda.java
@@ -1,4 +1,11 @@
 package data;
 
 public class CuentaVivienda {
+    private long precioVivienda;
+    private boolean vis;
+
+    public CuentaVivienda(long precioVivienda, boolean vis) {
+        this.precioVivienda = precioVivienda;
+        this.vis = vis;
+    }
 }
