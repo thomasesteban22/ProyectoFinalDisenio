102e624d1918b6831c65f0420920837da0e1d76b
thomasesteban22 <thomas.esteban@gmail.com>
2022-11-02 16:00:13 -0500

    test de actualizacion

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Prueba github");
-        public void;
-        /*
-        hgasdgshagdhs
-         */
+        /* test de actualizacion */
     }
 }
