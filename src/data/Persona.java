53365dbca7bc41f102a89895c9886199e6a24905
Manuel <manuel.castro01@correo.usa.edu.co>
2022-11-02 16:12:23 -0500

    clases persona

---
+++ src/data/Persona.java
@@ -0,0 +1,13 @@
+package data;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String correo;
+
+    public Persona(String nombre, String apellido, String correo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+}
