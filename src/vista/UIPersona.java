20d9b12e2e6bcc732c08e76775ab4e984c27f793
Manuel <manuel.castro01@correo.usa.edu.co>
2022-11-09 16:14:13 -0500

    VistaPersona

--- src/vista/UIPersona.java
+++ src/vista/UIPersona.java
@@ -4,4 +4,9 @@ import javax.swing.*;
 
 public class UIPersona {
     private JButton button1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton button2;
 }
