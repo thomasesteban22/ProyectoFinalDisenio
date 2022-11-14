84ebf527973dce52e14bb7bfbbe3791d4c8cc309
Manuel <manuel.castro01@correo.usa.edu.co>
2022-11-14 17:37:10 -0500

    Vista Cuenta

--- src/vista/UICuentas.java
+++ src/vista/UICuentas.java
@@ -1,4 +1,17 @@
 package vista;
 
+import javax.swing.*;
+
 public class UICuentas {
+    public JPanel vistaCuenta;
+    public JButton adicionarCuentaAhorros;
+    public JButton adicionarCuentaCorriente;
+    public JButton adicionarCuentaVivienda;
+    public JTextField numeroDeCuenta;
+    public JTextField fechaDeApertura;
+    public JTextField id;
+    public JTextField nombre;
+    public JTextField apellido;
+    public JTextField correo;
+    public JButton buscar;
 }
