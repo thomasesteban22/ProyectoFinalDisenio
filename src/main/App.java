8c0d30fad153e2389df28eb260e7a4377ddd7273
thomasesteban22 <thomas.esteban@gmail.com>
2022-11-13 21:24:15 -0500

    reparacion del main

--- src/main/App.java
+++ src/main/App.java
@@ -3,6 +3,8 @@ import controladores.ControladorPersona;
 import vista.UIPersona;
 
 
+
+
 public class App {
     public static void main(String[] args){
         ControladorPersona controladorPersona = new ControladorPersona(new UIPersona());
