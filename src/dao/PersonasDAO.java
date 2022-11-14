3fd1572be0655a61baab404c60a449fed5db7e16
thomasesteban22 <thomas.esteban@gmail.com>
2022-11-13 22:01:07 -0500

    reparacion despliegue de vista

--- src/dao/PersonasDAO.java
+++ src/dao/PersonasDAO.java
@@ -3,12 +3,18 @@ package dao;
 import data.Persona;
 
 
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PersonasDAO {
     private List<Persona> listaPersona;
-
+    //private ObjectInputStream entrada;
+    //private ObjectOutputStream salida;
+    //private String filePath = "person.dat";
     public void PersonasDAO() {
         listaPersona = new ArrayList<>();
     }
--- src/vista/UIPersona.java
+++ src/vista/UIPersona.java
@@ -2,11 +2,20 @@ package vista;
 
 import javax.swing.*;
 
-public class UIPersona {
+public class UIPersona extends JFrame {
     public JButton adicionar;
     public JButton buscar;
     public JTextField id;
     public JTextField nombre;
     public JTextField apellido;
     public JTextField correo;
+    private JPanel vistaPersona;
+
+    public UIPersona(){
+        setContentPane(vistaPersona);
+        setTitle("Vista persona");
+        setSize(450, 450);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
