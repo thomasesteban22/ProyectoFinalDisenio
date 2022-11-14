2a4ae2f40f7e30c1ad4cd96a07163b16fa0a8903
thomasesteban22 <thomas.esteban@gmail.com>
2022-11-13 20:33:31 -0500

    correccion de errores

--- src/controladores/ControladorPersona.java
+++ src/controladores/ControladorPersona.java
@@ -1,4 +1,17 @@
 package controladores;
+import dao.PersonasDAO;
+import vista.*;
 
 public class ControladorPersona {
+    private UIPersona vista;
+    private PersonasDAO modelo;
+
+
+    public ControladorPersona(UIPersona vista){
+        this.vista = vista;
+        this.modelo = new PersonasDAO();
+
+    }
+
 }
+
