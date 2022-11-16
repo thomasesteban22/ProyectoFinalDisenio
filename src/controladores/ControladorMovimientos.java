52702fe401e771c47501d1f57a3ccd42df5b1b49
Manuel <manuel.castro01@correo.usa.edu.co>
2022-11-16 17:49:55 -0500

    UIMovimientos

---
+++ src/controladores/ControladorMovimientos.java
@@ -0,0 +1,39 @@
+package controladores;
+
+import dao.CuentasDAO;
+
+import dao.PersonasDAO;
+import data.CuentaCorriente;
+import data.CuentaVivienda;
+import vista.UICuentas;
+import vista.UIMovimientos;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class ControladorMovimientos implements ActionListener {
+    private UIMovimientos vista;
+    private CuentasDAO modelo;
+
+    public ControladorMovimientos(UIMovimientos vista) throws IOException {
+        this.vista = vista;
+        this.modelo = new CuentasDAO();
+        this.vista.buscar.addActionListener(this);
+        this.vista.depositar.addActionListener(this);
+        this.vista.retirar.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(vista.buscar)){
+
+        }
+        if(e.getSource().equals(vista.depositar)){
+            CuentaCorriente cuentaCorriente =new CuentaCorriente();
+        }
+        if(e.getSource().equals(vista.retirar)) {
+        }
+
+    }
+}
