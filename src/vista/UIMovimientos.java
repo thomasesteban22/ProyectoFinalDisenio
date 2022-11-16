ac9bbb5f2efe43931edb2f291759b81313e20576
Manuel <manuel.castro01@correo.usa.edu.co>
2022-11-16 17:20:46 -0500

    UIMovimientos

--- src/vista/UIMovimientos.java
+++ src/vista/UIMovimientos.java
@@ -1,4 +1,13 @@
 package vista;
 
+import javax.swing.*;
+
 public class UIMovimientos {
+    private JTextField textField1;
+    private JButton buscarButton;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton depositarButton;
+    private JButton retirarButton;
+    private JTextField textField4;
 }
